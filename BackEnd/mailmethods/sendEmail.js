2436de4769e56627b068235eb02609c7557a3bbb
Gaurav-Sahni-17 <sahnigaurav17@gmail.com>
2025-06-06 18:29:41 +0530

    Security

--- BackEnd/mailmethods/sendEmail.js
+++ BackEnd/mailmethods/sendEmail.js
@@ -1,16 +1,17 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail', 
   auth: {
-    user: "sahnigaurav17@gmail.com", 
-    pass: "exvbfiiyppylftzu",
+    user: process.env.MAIL, 
+    pass: process.env.MAIL_PASS,
   },
 });
 
 const sendEmail = async (to, subject, text) => {
   const mailOptions = {
-    from: `"Chat App" <"sahnigaurav17@gmail.com">`,
+    from: `"Chat App" <${process.env.MAIL}>`,
     to: to,
     subject: subject,
     text: text,
